5dd425ae1546a1b4356038fc3238808d16289679
Tiago Moreira <tsm_projetos@hotmail.com>
2020-03-19 23:42:58 -0300

    publicação do buid

---
+++ precache-manifest.4c55fb35f1126b83f6b0f7e3fead5d51.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b332d1d39c7269887e99b0bc67a2973",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c697fa3560a2decf0fd5",
+    "url": "/static/css/main.87496bf7.chunk.css"
+  },
+  {
+    "revision": "d94464e8efb9862fe368",
+    "url": "/static/js/2.87d52b97.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.87d52b97.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c697fa3560a2decf0fd5",
+    "url": "/static/js/main.53d9b08b.chunk.js"
+  },
+  {
+    "revision": "880f8ac727ed99fbf2d0",
+    "url": "/static/js/runtime-main.4d65ef45.js"
+  },
+  {
+    "revision": "288302ea531af8be59f6ac2b5bbbfdd3",
+    "url": "/static/media/RobotoMono-Thin.288302ea.ttf"
+  }
+]);
